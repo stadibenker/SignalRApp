4720d18961b14932c5b8f46c186f7eb41bc90567
Maxim Bazhenov <stadibenker@gmail.com>
2024-10-21 11:50:03 +0100

    added hub

--- Hubs/UserHub.cs
+++ Hubs/UserHub.cs
@@ -4,5 +4,13 @@ namespace SignalRApp.Hubs
 {
 	public class UserHub : Hub
 	{
+		public static int TotalViews { get; set; } = 0;
+
+		public async Task NewWindowLoaded()
+		{
+			TotalViews++;
+
+			await Clients.All.SendAsync("updateTotalViews", TotalViews);
+		}
 	}
 }
